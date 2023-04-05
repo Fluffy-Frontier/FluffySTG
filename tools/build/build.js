14885ab85d500291807aafcdf2a1c5ecf44a3576
Vladimir Veisman <v.veisman@flashie.me>
2023-04-05 23:19:44 +0300

    Folder fol modular changes

--- tools/build/build.js
+++ tools/build/build.js
@@ -76,6 +76,7 @@ export const DmTarget = new Juke.Target({
     'icons/**',
     'interface/**',
     'modular_skyrat/**', ///SKYRAT EDIT ADDITION - Making the CBT work
+    'ff/**',
     `${DME_NAME}.dme`,
     NamedVersionFile,
   ],
