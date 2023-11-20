fbee9a5e36104c6dd1ce5dc90c5ad04da1ae42f4
Iajret Creature <122297233+Steals-The-PRs@users.noreply.github.com>
2023-11-20 15:13:15 +0300

    [MIRROR] Partial revert of "Heretic Rebalance - Main Knowledge gives free Side points" (the side path part) [MDB IGNORE] (#720)
    * Partial revert of "Heretic Rebalance - Main Knowledge gives free Side points" (the side path part)
    
    * Update heretic_antag.dm
    
    * Update heretic_antag.dm
    
    ---------
    
    Co-authored-by: SkyratBot <59378654+SkyratBot@users.noreply.github.com>
    Co-authored-by: MrMelbert <51863163+MrMelbert@users.noreply.github.com>
    Co-authored-by: Bloop <13398309+vinylspiders@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/AntagInfoHeretic.tsx
+++ tgui/packages/tgui/interfaces/AntagInfoHeretic.tsx
@@ -46,7 +46,6 @@ type KnowledgeInfo = {
 
 type Info = {
   charges: number;
-  side_charges: number;
   total_sacrifices: number;
   ascended: BooleanLike;
   objectives: Objective[];
@@ -180,7 +179,7 @@ const GuideSection = () => {
 
 const InformationSection = (props, context) => {
   const { data } = useBackend<Info>(context);
-  const { charges, side_charges, total_sacrifices, ascended } = data;
+  const { charges, total_sacrifices, ascended } = data;
   return (
     <Stack.Item>
       <Stack vertical fill>
@@ -202,13 +201,6 @@ const InformationSection = (props, context) => {
           <span style={hereticBlue}>
             knowledge point{charges !== 1 ? 's' : ''}
           </span>
-          {!!side_charges && (
-            <span>
-              {' '}
-              and <b>{side_charges}</b> side point
-              {side_charges !== 1 ? 's' : ''}
-            </span>
-          )}{' '}
           .
         </Stack.Item>
         <Stack.Item>
@@ -283,7 +275,7 @@ const KnowledgeShop = (props, context) => {
 
 const ResearchInfo = (props, context) => {
   const { data } = useBackend<Info>(context);
-  const { charges, side_charges } = data;
+  const { charges } = data;
 
   return (
     <Stack justify="space-evenly" height="100%" width="100%">
@@ -293,14 +285,7 @@ const ResearchInfo = (props, context) => {
             You have <b>{charges || 0}</b>&nbsp;
             <span style={hereticBlue}>
               knowledge point{charges !== 1 ? 's' : ''}
-            </span>
-            {!!side_charges && (
-              <span>
-                {' '}
-                and <b>{side_charges}</b> side point
-                {side_charges !== 1 ? 's' : ''}
-              </span>
-            )}{' '}
+            </span>{' '}
             to spend.
           </Stack.Item>
           <Stack.Item grow>
