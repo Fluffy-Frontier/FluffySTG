2835544d9381a1314c72201079e7843caa6c665f
SkyratBot <59378654+SkyratBot@users.noreply.github.com>
2023-12-31 01:52:31 +0100

    [MIRROR] Fixes TextArea not updating on props change [MDB IGNORE] (#25920)
    * Fixes TextArea not updating on props change (#80656)
    
    * Fixes TextArea not updating on props change
    
    ---------
    
    Co-authored-by: Bloop <13398309+vinylspiders@users.noreply.github.com>

--- tgui/packages/tgui/components/TextArea.tsx
+++ tgui/packages/tgui/components/TextArea.tsx
@@ -107,7 +107,10 @@ export const TextArea = forwardRef(
       () => textareaRef.current as HTMLTextAreaElement,
     );
 
+    /** Focuses the input on mount */
     useEffect(() => {
+      if (!autoFocus && !autoSelect) return;
+
       const input = textareaRef.current;
       if (!input) return;
 
@@ -123,6 +126,17 @@ export const TextArea = forwardRef(
         }, 1);
       }
     }, []);
+
+    /** Updates the initial value on props change */
+    useEffect(() => {
+      const input = textareaRef.current;
+      if (!input) return;
+
+      const newValue = toInputValue(value);
+      if (input.value === newValue) return;
+
+      input.value = newValue;
+    }, [value]);
 
     return (
       <Box
