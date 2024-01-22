Skyrat-SS13/Skyrat-tg

Gags croptop (#26179)
* Update coats.dm

* Update greyscale_configs.dm

* Add files via upload

* Add files via upload

* Add files via upload

* Update loadout_datum_suit.dm

* Update coats.dm

modular_skyrat/modules/GAGS/greyscale_configs.dm
@@ -796,6 +796,17 @@
 	name = "Varsity Jacket (Worn)"
 	json_config = 'modular_skyrat/modules/GAGS/json_configs/suits/varsity/varsity_worn.json'
 
+//CROPTOP SWEATER
+
+/datum/greyscale_config/croptop
+	name = "Croptop Sweater"
+	icon_file = 'modular_skyrat/master_files/icons/mob/clothing/suit.dmi'
+	json_config = 'modular_skyrat/modules/GAGS/json_configs/suits/croptop/croptop.json'
+
+/datum/greyscale_config/croptop/worn
+	name = "Croptop Sweater (Worn)"
+	json_config = 'modular_skyrat/modules/GAGS/json_configs/suits/croptop/croptop_worn.json'
+
 // TAILORED JACKET
 
 /datum/greyscale_config/tailored_jacket

Fluffy-Frontier/FluffySTG

modular_nova/modules/GAGS/greyscale_configs.dm
@@ -796,6 +796,17 @@
 	name = "Varsity Jacket (Worn)"
 	json_config = 'modular_nova/modules/GAGS/json_configs/suits/varsity/varsity_worn.json'
 
+//CROPTOP SWEATER
+
+/datum/greyscale_config/croptop
+	name = "Croptop Sweater"
+	icon_file = 'modular_nova/master_files/icons/mob/clothing/suit.dmi'
+	json_config = 'modular_nova/modules/GAGS/json_configs/suits/croptop/croptop.json'
+
+/datum/greyscale_config/croptop/worn
+	name = "Croptop Sweater (Worn)"
+	json_config = 'modular_nova/modules/GAGS/json_configs/suits/croptop/croptop_worn.json'
+
 // TAILORED JACKET
 
 /datum/greyscale_config/tailored_jacket