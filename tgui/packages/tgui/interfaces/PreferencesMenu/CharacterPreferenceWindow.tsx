2f24dc08c17764f0b31d1cef7c5f2922b93464ec
Yaroslav Nurkov <nurkov12@gmail.com>
2024-06-18 21:54:27 +0300

    initial

--- tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferenceWindow.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferenceWindow.tsx
@@ -2,7 +2,7 @@ import { exhaustiveCheck } from 'common/exhaustive';
 import { useState } from 'react';
 
 import { useBackend } from '../../backend';
-import { Dropdown, Flex, Stack } from '../../components'; // NOVA EDIT CHANGE - ORIGINAL: import { Stack } from '../../components';
+import { Button, Dropdown, Flex, Stack } from '../../components'; // NOVA EDIT CHANGE - ORIGINAL: import { Stack } from '../../components';
 import { Window } from '../../layouts';
 import { AntagsPage } from './AntagsPage';
 import { PreferencesMenuData } from './data';
@@ -31,10 +31,11 @@ enum Page {
 const CharacterProfiles = (props: {
   activeSlot: number;
   onClick: (index: number) => void;
+  onButtonClick: (e: Event) => void; // TFF ADDITION - Character Deletion
   profiles: (string | null)[];
 }) => {
-  const { profiles, activeSlot, onClick } = props; // NOVA EDIT CHANGE
-
+  const { profiles, activeSlot, onClick, onButtonClick } = props; // NOVA EDIT CHANGE // TFF EDIT - Character Deletion
+  let charLength = Object.values(profiles).filter((val) => val).length; // TFF ADDITION - Character Deletion
   return (
     <Flex /* NOVA EDIT CHANGE START - Nova uses a dropdown instead of buttons */
       align="center"
@@ -54,6 +55,26 @@ const CharacterProfiles = (props: {
           }}
         />
       </Flex.Item>
+      {/* TFF ADDITION - Character Deletion */}
+      <Flex.Item>
+        <Button
+          color="danger"
+          icon="trash"
+          width="1.7777rem"
+          height="1.7777rem"
+          ml="25%"
+          disabled={charLength < 2}
+          tooltip={
+            charLength < 2
+              ? "You can't delete a character if you only have one."
+              : 'Delete a character'
+          }
+          onClick={(e) => {
+            onButtonClick(e);
+          }}
+        />
+      </Flex.Item>
+      {/* TFF ADDITION END - Character Deletion */}
     </Flex> /* NOVA EDIT CHANGE END */
   );
 };
@@ -109,6 +130,27 @@ export const CharacterPreferenceWindow = (props) => {
               onClick={(slot) => {
                 act('change_slot', {
                   slot: slot + 1,
+                });
+              }}
+              onButtonClick={(e) => {
+                let choicedSlot = data.active_slot - 1;
+                let availableSlots = Object.keys(data.character_profiles)
+                  .filter((slot) => data.character_profiles[slot] !== null)
+                  .map((slot) => parseInt(slot, 10)); // get all busy slots as num
+
+                availableSlots.splice(availableSlots.indexOf(choicedSlot), 1); // remove our slot(which we want to delete)
+                if (availableSlots?.length < 2) return;
+                let slotToJump = availableSlots.reduce((prev, curr) => {
+                  return Math.abs(curr - choicedSlot) <
+                    Math.abs(prev - choicedSlot)
+                    ? curr
+                    : prev;
+                }); // get nearest slot
+
+                act('remove_slot', {
+                  slot: data.active_slot,
+                  name: Object.values(data.character_profiles)[choicedSlot],
+                  slotToJump: slotToJump + 1,
                 });
               }}
               profiles={data.character_profiles}
