3ebd80b50e21614172233d8e7f98a69d6ab652d0
Kocma-san <kocmasan@gmail.com>
2025-05-18 17:24:39 +0700

    something

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/antag_optin.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/antag_optin.tsx
@@ -9,6 +9,6 @@ export const antag_opt_in_status_pref: FeatureChoiced = {
     By extension, picking "Round Remove" will allow you to be round removed in applicable situations, even by non-antagonists. \
     Enabling any non-ghost antags \
     (revenant, abductor contractor, etc.) will force your opt-in to be, \
-    at minimum, "Temporarily Inconvenience".',
+    at minimum, "Kill".', // FLUFFY FRONTIER EDIT - ORIGNIAL: at minimum, "Temporarily Inconvenience".',
   component: FeatureDropdownInput,
 };
